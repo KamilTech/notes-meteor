c400c917e7f24a171561b995c4db61bf47f354a9
Kamil <kamil.mich50@gmail.com>
2018-08-21 15:09:29 +0200

    editor input and textarea

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -2,15 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Session } from 'meteor/session';
+import { Meteor } from 'meteor/meteor';
 
-import Notes from '../api/notes';
+import { Notes } from '../api/notes';
 
 export class Editor extends React.Component {
+    handleBodyChange(e) {
+        this.props.call('notes.update', this.props.note._id, {
+            body: e.target.value
+        });
+    }
+
+    handleTitleChange(e) {
+        this.props.call('notes.update', this.props.note._id, {
+            title: e.target.value
+        });
+    }
+
     render() {
         if (this.props.note) {
-            return ( 
-            <p>We got the note!</p> 
-        );
+            return (
+                <div>
+                    <input value={this.props.note.title} placeholder="Your title here" onChange={this.handleTitleChange.bind(this)}/>
+                    <textarea value={this.props.note.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+                    <button>Delete Note</button>
+                </div>
+            );
         } else {
             return ( <p> { this.props.selectedNoteId ? 'Note not found' : 'Pick or create note to get started!' } </p> );
         }
@@ -27,6 +44,7 @@ export default withTracker(() => {
 
     return { 
         selectedNoteId,
-        note: Notes.findOne(selectedNoteId)
+        note: Notes.findOne(selectedNoteId),
+        call: Meteor.call
      };
 })(Editor);
