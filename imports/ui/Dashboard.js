38ad3cd7443855d10e39e65f91f9d1e148d582b5
Kamil <kamil.mich50@gmail.com>
2018-08-21 14:01:03 +0200

    Selected Note

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 
 import PrivateHeader from './PrivateHeader';
 import NoteList from './NoteList';
 
-export default class Link extends React.Component {
+export default class Dashboard extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
     componentWillMount() {
-      if (!Meteor.userId())
-          this.props.history.replace('/login');
+      if (!Meteor.userId()) {
+        this.props.history.replace('/login');
+      } else {
+        Session.set('selectedNoteId', this.props.match.params.id)
+      }
     }
 
     render() {
