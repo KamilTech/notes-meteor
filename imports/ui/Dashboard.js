2bc6fe848efcbb32f1b737f585cc5714ff9c9bee
Kamil <kamil.mich50@gmail.com>
2018-08-22 21:09:32 +0200

    logout errors

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { withRouter } from 'react-router'
 
 import PrivateHeader from './PrivateHeader';
 import NoteList from './NoteList';
 import Editor from './Editor';
 
-export default class Dashboard extends React.Component {
+class Dashboard extends React.Component {
     constructor(props) {
       super(props);
     }
 
+    componentDidMount() {
+      if (!Meteor.userId() && this.props.match.params.id) {
+        this.props.history.replace('/login');
+      };
+    }
+
     componentWillMount() {
+      if (Meteor.userId()) {
+        Session.set('selectedNoteId', this.props.match.params.id);
+      } else {
+        if (this.props.match.params.id) {
+          this.props.history.replace('/login');
+        }
+      }
+    }
+
+    componentWillReceiveProps() {
       if (!Meteor.userId()) {
         this.props.history.replace('/login');
-      } else {
-        Session.set('selectedNoteId', this.props.match.params.id)
-      }
+      };
     }
 
     render() {
@@ -30,3 +45,5 @@ export default class Dashboard extends React.Component {
       )
     }
   }
+
+  export default withRouter(Dashboard);
